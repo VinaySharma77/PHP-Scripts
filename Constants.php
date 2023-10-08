05de178c9d11a6717e545b1656702f654c83c766
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-10-08 18:45:16 +0530

    Add files via upload
    This is a PHP Script that shows how constants works.

---
+++ Constants.php
@@ -0,0 +1,8 @@
+<?php
+	//Constant in PHP
+	const num = 10;
+	echo "Constants in PHP";
+	echo "Value of num variable is : " . num;
+	num = 20; //This line will generate an error if we reassign value to constant
+	echo "After reassigning value of num is : " . num;
+?>
