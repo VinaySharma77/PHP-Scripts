aedf284534f8e01901eacbae73d7aa86f66160b2
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-10-08 18:12:45 +0530

    Add files via upload
    This is a PHP Script to show how PHP Comments work.

---
+++ Comments.php
@@ -0,0 +1,10 @@
+<?php
+	//This is Single Line Comment in PHP
+
+	/*
+	This is Multi-Line Comment in PHP
+	*/
+	
+	//Both Single and Multi-Line Comments are not executed.
+	//Comments are not a part of the program.
+?>
