cb4c10adc4f293e60e309c45678e2baa550c1b92
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-10-08 18:23:22 +0530

    Add files via upload
    This is a PHP Script that shows how we create Variables of different Data Types.

---
+++ DataTypes.php
@@ -0,0 +1,9 @@
+<?php
+	//We do not have to specify the data type of variable in PHP
+	$num = 10;
+	$text = "PHP Script";
+	$day = true;
+	$rupee = 10.5;
+	echo "PHP Data Types<br>";
+	echo "Number is : $num<br>Text is : $text<br>Day is : $day<br>Rupee is : $rupee";
+?>
