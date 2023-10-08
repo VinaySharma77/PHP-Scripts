49859369b34737ab0f595dcec905a948407bcceb
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-10-08 18:57:33 +0530

    Add files via upload
    This is a PHP Script that shows how Assignment Operator works.

---
+++ AssignmentOperator.php
@@ -0,0 +1,9 @@
+<?php
+	//Assignment Operator assigns some value to variable
+	$text = "Good Day";
+	$num = 10;
+	$time = true;
+	$cost = 20.2;
+	echo "PHP Assignment Operator<br>";
+	echo "Text = $text<br>Number : $num<br>Time is : $time<br>Cost is : $cost";
+?>
