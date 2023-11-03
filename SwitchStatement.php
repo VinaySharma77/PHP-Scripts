d8862a11ba8e5d65fa7b172759df292a8d5d5cea
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-03 12:22:30 +0530

    Add files via upload
    This is a PHP Script to show how Switch Statement works.

---
+++ SwitchStatement.php
@@ -0,0 +1,27 @@
+<?php
+    //PHP Switch Statement Example
+    $day = 1;
+    switch($day){
+        case 1:
+            echo "Day is : Monday<br>";
+            break;
+        case 2:
+            echo "Day is : Tuesday<br>";
+            break;
+        case 3:
+            echo "Day is : Wednesday<br>";
+            break;
+        case 4:
+            echo "Day is : Thursday<br>";
+            break;
+        case 5:
+            echo "Day is : Friday<br>";
+            break;
+        case 6:
+            echo "Day is : Saturday<br>";
+            break;
+        case 7:
+            echo "Day is : Sunday<br>";
+            break;
+    }
+?>
