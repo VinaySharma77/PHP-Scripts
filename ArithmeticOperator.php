6ffae84497ed1522fb2b9096c2401cced3edfee6
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-03 11:07:25 +0530

    Add files via upload
    This is a PHP Script that shows how Arithmetic Operators work on variables.

---
+++ ArithmeticOperator.php
@@ -0,0 +1,15 @@
+<?php
+	//Arithmetic Operator perform arithmetic operations on variables.
+	echo "Arithmetic Operator Example<br>";
+	$num1 = 10;
+	$num2 = 5;
+	$sum = $num1 + $num2;
+	$multiply = $num1 * $num2;
+	$divide = $num1 / $num2;
+	$subtract = $num1 - $num2;
+	echo "variable \$num1 = $num1 and \$num2 = $num2<br>";
+	echo "Sum is : $sum<br>";
+	echo "Multiplication is : $multiply<br>";
+	echo "Division is : $divide<br>";
+	echo "Subtraction is : $subtract<br>";
+?>
