ee66068f0a8d16f37520579b3c0068145323c4c4
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-03 11:21:35 +0530

    Add files via upload
    This is a PHP Script to show how Comparision Operator work on variables.

---
+++ ComparisionOperator.php
@@ -0,0 +1,15 @@
+<?php
+    //Comparision Operator Compares the value of variables
+    $num1 = 10;
+    $num2 = 5;
+    echo "\$num1 = $num1 and \$num2 = $num2<br>";
+    if($num1 == $num2){
+        echo "Both numbers are equal";
+    }
+    else if($num1 > $num2){
+        echo "\$num1 is greater";
+    }
+    else{
+        echo "\$num2 is greater";
+    }
+?>
