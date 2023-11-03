8be95a3f6d8486751ba12f856ad9d3616af6e8c0
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-03 12:13:19 +0530

    Add files via upload
    This is a PHP Script to show how Nested For Loop works.

---
+++ NestedForLoop.php
@@ -0,0 +1,9 @@
+<?php
+    //PHP Nested For Loop Example
+    for($i = 0; $i < 3 ; $i++){
+        echo "Outer For Loop Count $i<br>";
+        for($j = 0; $j < 10; $j++){
+            echo "Inner For Loop Count $j<br>";
+        }
+    }    
+?>
