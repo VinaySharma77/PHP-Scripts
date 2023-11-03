bf6ab099e07de7db998ad857008deb74b9306b5f
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-03 12:26:38 +0530

    Add files via upload
    This is a PHP Script to show how Break Statement works.

---
+++ BreakStatement.php
@@ -0,0 +1,10 @@
+<?php
+    //PHP Break Statement Example
+    for($i = 0 ;$i < 10; $i++){
+        if($i == 6){
+            echo "Break Statement Occurs";
+            break;
+        }
+        echo "For Loop Executed<br>";
+    }
+?>
