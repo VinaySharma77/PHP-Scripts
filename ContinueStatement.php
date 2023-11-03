650cae3ef4083f7f692be9281a6f1cf363478b9f
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-03 12:28:47 +0530

    Add files via upload
    This is a PHP Script to show how Continue Statement works.

---
+++ ContinueStatement.php
@@ -0,0 +1,10 @@
+<?php
+    //PHP Continue Statement Example
+    for($i = 0 ;$i < 10; $i++){
+        if($i == 6){
+            echo "Continue Statement Occurs<br>";
+            continue;
+        }
+        echo "For Loop Executed<br>";
+    }
+?>
