5f9bb05bb216c23c9ab3e0305daf145bf503e6ee
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-06 10:29:07 +0530

    Add files via upload
    This is a PHP Script to Search Position of a String.

---
+++ StringSearch.php
@@ -0,0 +1,6 @@
+<?php
+    //PHP Script to Search Position of a String .
+    $txt = "Good Day";
+    $search_txt = strpos($txt, "Day");
+    echo "Position of \"Day\" String in \"Good Day\" String is : $search_txt";
+?>
