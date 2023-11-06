058ffe802b7d11f03849e1c92457d93556c994a4
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-06 11:42:05 +0530

    Add files via upload
    This is a PHP Script that shows how to Create and Print Associative Array.

---
+++ AssociativeArray.php
@@ -0,0 +1,8 @@
+<?php
+    //PHP Associative Array
+    $my_cars = array("Ferrari"=>"first", "Supra"=>"second", "Lamborghini"=>"third", "BMW"=>"fourth");
+    echo "Ferrari Car is : " . $my_cars["Ferrari"] . "<br>";
+    echo "Supra Car is : " . $my_cars["Supra"] . "<br>";
+    echo "Lamborghini Car is : " . $my_cars["Lamborghini"] . "<br>";
+    echo "BMW Car is : " . $my_cars["BMW"];
+?>
