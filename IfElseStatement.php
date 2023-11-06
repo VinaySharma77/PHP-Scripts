790a6917940d6ab4b0623097852bc2e318abf005
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-06 11:14:28 +0530

    Add files via upload
    This is a PHP Script that shows how If Else Statement works.

---
+++ IfElseStatement.php
@@ -0,0 +1,12 @@
+<?php
+    //PHP If Else Statement Example
+    $age = 15;
+    if($age > 18){
+        echo "Your age is $age<br>";
+        echo "You are eligible to vote.";
+    }
+    else{
+        echo "Your age is $age<br>";
+        echo "You are not eligible to vote.";
+    }
+?>
