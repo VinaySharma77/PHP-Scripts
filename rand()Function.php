227d1c288dd1393034505b49f11ab79dc0f4b4df
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-06 11:08:22 +0530

    Add files via upload
    This is a PHP Script that shows how to generate a Random Number.

---
+++ rand()Function.php
@@ -0,0 +1,6 @@
+<?php
+    //PHP rand() Function to generate a random number
+    $random_num = rand();
+    echo "Random Number is :  $random_num<br>";
+    echo "Random number will generate every time when you refresh the browser.";
+?>
