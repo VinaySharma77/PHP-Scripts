6a94a68371a80d923588140555679a0fe14d9cb5
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-06 11:24:45 +0530

    Add files via upload
    This is a PHP Script that shows how to store multiple values in an Array.

---
+++ Arrays.php
@@ -0,0 +1,8 @@
+<?php
+    //PHP Arrays
+    $my_cars = array("Ferrari", "Supra", "Lamborghini", "BMW");
+    $my_fav = $my_cars[1];
+    echo "My Car Collection is : ";
+    echo $my_cars[0] . ", " . $my_cars[1] . ", " . $my_cars[2] . ", "  . $my_cars[3] . "<br>";
+    echo "My Favourite Car is : $my_fav";
+?>
