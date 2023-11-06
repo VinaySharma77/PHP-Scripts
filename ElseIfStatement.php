72b35944384b1feafffbe12ad108b17374bd353a
Vinay Sharma <86404594+VinaySharma77@users.noreply.github.com>
2023-11-06 11:17:25 +0530

    Add files via upload
    This is a PHP Script that shows how Else If Statement works.

---
+++ ElseIfStatement.php
@@ -0,0 +1,13 @@
+<?php
+    //PHP Else If Statement Example
+    $num = 20;
+    if($num > 20){
+        echo "Number is greater than 20";
+    }
+    else if($num == 20){
+        echo "Number is 20";
+    }
+    else{
+        echo "Number is less than 20";
+    }
+?>
